c59cf96dd876a1d52065ea63a71049a7727c3a12
cena <cena@seektopser.com>
2019-12-18 15:00:40 +0800

    optimization

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -34,7 +34,11 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader'
+            loader: 'babel-loader',
+            options: {
+              // 使用vue官方的懒加载语法并结合babel需使用这个插件，不然会报错
+              plugins: ["@babel/plugin-syntax-dynamic-import"]
+            }
           }
         ]
       },
@@ -153,5 +157,27 @@ module.exports = {
     // 添加一个 resolve.extensions 属性，方便我们引入依赖或者文件的时候可以省略后缀
     // 我们在引入文件时可以这样写 import api from '@/api/api'。
     extensions: ['*', '.js', '.vue']
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      cacheGroups: {
+        // node_modules下的模块拆分到chunk-vendors.xxxx.js下
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\\/]node_modules[\\\/]/,
+          priority: -10,
+          chunks: 'all'
+        },
+        // 自己定义的公告组件超过两次引用的放在chunk-common.xxxx.js下
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          priority: -20,
+          chunks: 'all',
+          reuseExistingChunk: true
+        }
+      }
+    }
   }
 }
